f83be699bc60fd50d518ff5542a13df65f4cbe3b
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-07-04 17:20:24 +0530

    car class inherit from vehicle

---
+++ src/Car.java
@@ -0,0 +1,30 @@
+public class Car extends Vehicle {
+
+    private int wheels;
+    private String type;
+    private int doors;
+    private int gears;
+    private boolean isManual;
+    private int currentGear;
+
+
+    public Car(String name, String type, int wheels, int doors, int gears, boolean isManual) {
+        super(name);
+        this.type = type;
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        currentGear = 1;
+    }
+
+    public void changeGear(int newGear) {
+        this.currentGear = newGear;
+        System.out.println("change method called: Change to" +currentGear+ "gear.");
+    }
+
+    public void changeSpeed(int newSpeed, int newDirection) {
+        move(newSpeed,newDirection);
+        System.out.println("Change speed called: Speed -> " +newSpeed+ "Direction -> "+newDirection);
+    }
+}
---
+++ src/SUV.java
@@ -0,0 +1,7 @@
+public class SUV extends Car {
+
+    public SUV(String name, int gears, boolean isManual) {
+        super(name, "SUV", 4, 5, gears, isManual);
+    }
+}
+}
